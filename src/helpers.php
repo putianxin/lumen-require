088a9a7eaf815320bb83f430bc60b23babb03b38
putianxin@shanlaan <382820748@qq.com>
2018-10-16 14:55:51 +0800

    增加resolve（适配email导出）

--- src/helpers.php
+++ src/helpers.php
@@ -146,3 +146,14 @@ if(!function_exists('getReportTime')) {
         return false;
     }
 }
+if (!function_exists('resolve')) {
+    /**
+     * Resolve a service from the container.
+     * @param  string $name
+     * @return mixed
+     */
+    function resolve($name)
+    {
+        return app($name);
+    }
+}
