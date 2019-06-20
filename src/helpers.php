b4b17dc73d1cf8859927bfe07edf115f1d73f10c
天鑫 <382820748@qq.com>
2019-06-20 20:33:11 +0800

    save

--- src/helpers.php
+++ src/helpers.php
@@ -138,7 +138,7 @@ if (!function_exists('exception')) {
      */
     function exception($message, $code = 500)
     {
-        throw new AuthorizationException($message, $code);
+        throw new ValidationException($message, $code);
     }
 }
 
