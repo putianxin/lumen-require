ce8424994bcfd55b44b34a479104cb11f206bf13
天鑫 <382820748@qq.com>
2019-08-06 12:07:36 +0800

    save

--- src/helpers.php
+++ src/helpers.php
@@ -138,7 +138,7 @@ if (!function_exists('exception')) {
      */
     function exception($message, $code = 500)
     {
-        abort($code,$message);
+        abort($code, $message);
     }
 }
 
@@ -198,7 +198,7 @@ if (!function_exists('numberToBitArray')) {
             }
             $number = $number >> 1;
         }
-        if(!$return){
+        if (!$return) {
             $return = null;
         }
         return $return;
@@ -221,12 +221,12 @@ if (!function_exists('bitJsonToNumber')) {
     }
 }
 
-if (! function_exists('mix')) {
+if (!function_exists('mix')) {
     /**
      * Get the path to a versioned Mix file.
      *
-     * @param  string  $path
-     * @param  string  $manifestDirectory
+     * @param  string $path
+     * @param  string $manifestDirectory
      * @return \Illuminate\Support\HtmlString|string
      *
      * @throws \Exception
@@ -235,28 +235,28 @@ if (! function_exists('mix')) {
     {
         static $manifests = [];
 
-        if (! Str::startsWith($path, '/')) {
+        if (!Str::startsWith($path, '/')) {
             $path = "/{$path}";
         }
 
-        if ($manifestDirectory && ! Str::startsWith($manifestDirectory, '/')) {
+        if ($manifestDirectory && !Str::startsWith($manifestDirectory, '/')) {
             $manifestDirectory = "/{$manifestDirectory}";
         }
 
-        if (file_exists(public_path($manifestDirectory.'/hot'))) {
-            $url = file_get_contents(public_path($manifestDirectory.'/hot'));
+        if (file_exists(public_path($manifestDirectory . '/hot'))) {
+            $url = file_get_contents(public_path($manifestDirectory . '/hot'));
 
             if (Str::startsWith($url, ['http://', 'https://'])) {
-                return new HtmlString(Str::after($url, ':').$path);
+                return new HtmlString(Str::after($url, ':') . $path);
             }
 
             return new HtmlString("//localhost:8080{$path}");
         }
 
-        $manifestPath = public_path($manifestDirectory.'/mix-manifest.json');
+        $manifestPath = public_path($manifestDirectory . '/mix-manifest.json');
 
-        if (! isset($manifests[$manifestPath])) {
-            if (! file_exists($manifestPath)) {
+        if (!isset($manifests[$manifestPath])) {
+            if (!file_exists($manifestPath)) {
                 throw new Exception('The Mix manifest does not exist.');
             }
 
@@ -265,29 +265,29 @@ if (! function_exists('mix')) {
 
         $manifest = $manifests[$manifestPath];
 
-        if (! isset($manifest[$path])) {
+        if (!isset($manifest[$path])) {
             report(new Exception("Unable to locate Mix file: {$path}."));
 
-            if (! app('config')->get('app.debug')) {
+            if (!app('config')->get('app.debug')) {
                 return $path;
             }
         }
 
-        return new HtmlString($manifestDirectory.$manifest[$path]);
+        return new HtmlString($manifestDirectory . $manifest[$path]);
     }
 }
 
-if (! function_exists('report')) {
+if (!function_exists('report')) {
     /**
      * Report an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
      * @return void
      */
     function report($exception)
     {
         if ($exception instanceof Throwable &&
-            ! $exception instanceof Exception) {
+            !$exception instanceof Exception) {
             $exception = new FatalThrowableError($exception);
         }
 
